4fb1cf10ed29b77a80ab4824f0b0d31116395e86
Nikita <nikitamaryniuk@gmail.com>
2023-12-16 23:50:57 +0100

    16.12

--- MusicApp/src/App.jsx
+++ MusicApp/src/App.jsx
@@ -1,4 +1,9 @@
+import style from './global.module.scss'
 import MainPage from './pages/MainPage/MainPage'
-const App = () => <MainPage />
+const App = () => (
+	<div className={style.wrapper}>
+		<MainPage />
+	</div>
+)
 
 export default App
--- MusicApp/src/pages/MainPage/MainPage.jsx
+++ MusicApp/src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,3 @@
-const MainPage = () => <>Главная страница</>
+const MainPage = () => <div className=''>Главная страница</div>
 
 export default MainPage
