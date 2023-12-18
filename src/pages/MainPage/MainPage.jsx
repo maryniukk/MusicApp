0f6471916e947632587e122ba082e9788db331dc
Nikita <nikitamaryniuk@gmail.com>
2023-12-18 01:03:41 +0100

    input field ...

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,18 @@
 import { Input } from '@mui/material'
+import { useState } from 'react'
 import tracksList from '../../assets/tracksList.js'
 import Track from '../../components/Track/Track.jsx'
 import style from './MainPage.module.scss'
+const runSearch = (query) => {
+  if (!query) {
+    return tracksList
+  }
+}
 
 const MainPage = () => {
+  const [tracks, setTracks] = useState(tracksList)
   const handleChange = (event) => {
+    // Получаю при вводе в инпут результат в log.
     console.log(event.target.value)
   }
 
@@ -12,16 +20,20 @@ const MainPage = () => {
     <div className={style.search}>
       <Input className={style.input} placeholder="Track search" onChange={handleChange} />
       <div className={style.list}>
-        {tracksList.map((track) => (
-          <Track
-            key={track.id}
-            src={track.src}
-            preview={track.preview}
-            duration={track.duration}
-            title={track.title}
-            artists={track.artists}
-          />
-        ))}
+        {tracks.map(
+          (
+            track, //Мапим компоненту трек.
+          ) => (
+            <Track
+              key={track.id}
+              src={track.src}
+              preview={track.preview}
+              duration={track.duration}
+              title={track.title}
+              artists={track.artists}
+            />
+          ),
+        )}
       </div>
     </div>
   )
